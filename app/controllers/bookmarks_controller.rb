0369b46b5bcff2572905cfd64fbac312a93101cd
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-05-28 16:37:50 +0100

    added form inside list and delete functionality

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,19 +1,23 @@
 class BookmarksController < ApplicationController
   def new
-    @bookmark = Bookmark.new
-    @list = List.find(params[:list_id])
-    raise
+
   end
 
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmarks_params)
-    @bookmark.list_id = @list
+    @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
       render :new
     end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
